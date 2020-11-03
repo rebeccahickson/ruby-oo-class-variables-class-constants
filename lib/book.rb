86b08e0b30d1308a84e7507741c1e8319152fc70
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-03 09:44:16 -0500

    commit tree 6fb8c9de52f72b628195f576cc19c86b281a9621

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,19 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  # rubocop:disable Style/MutableConstant
+  GENRES = []
+  # rubocop:enable Style/MutableConstant
   def initialize(title)
     @title = title
   end
 
   def turn_page
-    puts "Flipping the page...wow, you read fast!"
+    puts 'Flipping the page...wow, you read fast!'
   end
 
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  # rubocop:disable Style/MutableConstant
+  BRANDS = []
+  # rubocop:enable Style/MutableConstant
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
 
   def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+    self.condition = 'new'
+    puts 'Your shoe is as good as new!'
   end
-
 end
